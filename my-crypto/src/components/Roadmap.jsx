6775f523062718c8d69461fd1cd365feb83c2711
unknown <ajibadeifedolapo17@gmail.com>
2023-06-15 19:50:31 +0100

    Building

--- my-crypto/src/components/Roadmap.jsx
+++ my-crypto/src/components/Roadmap.jsx
@@ -5,9 +5,9 @@ const Roadmap = () => {
     <div className='flex w-full h-full flex-col gap-10 bg-[#1A2156] py-6 px-12 items-center justify-center' id='ecosystem'>
 
 
-<h2 className='relative md:text-[87px] text-[30px] font-bold text-gray-700'>
+<h2 className='relative md:text-[87px] text-[30px] font-bold text-gray-700 dark:text-gray-800'>
 ROADMAP
-<h2 className='absolute md:text-[60px] text-base font-display font-semibold text-white md:top-16 md:left-24 top-4 left-10'>
+<h2 className='absolute md:text-[60px] text-base font-display font-semibold text-white dark:text-white md:top-16 md:left-24 top-4 left-10'>
     ROADMAP
     </h2>
 
--- my-crypto/src/components/Tokenomics.jsx
+++ my-crypto/src/components/Tokenomics.jsx
@@ -4,9 +4,9 @@ const Tokenomics = () => {
   return (
     <div className='flex w-full h-full flex-col gap-10 bg-[#1A2156] py-6 px-12 items-center justify-center'>
 
-<h2 className='relative md:text-[87px] text-[30px] font-bold text-gray-700 mt-[300px] md:mt-10'>
+<h2 className='relative md:text-[87px] text-[30px] font-bold text-gray-700 dark:text-gray-800 mt-[300px] md:mt-10'>
 TOKENOMICS
-<h2 className='absolute md:text-[60px] text-base font-display font-semibold text-white md:top-16 md:left-24 top-4 left-10'>
+<h2 className='absolute md:text-[60px] text-base font-display font-semibold text-white dark:text-white md:top-16 md:left-24 top-4 left-10'>
     TOKENOMICS
     </h2>
 
